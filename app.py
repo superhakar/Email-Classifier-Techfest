32d0d0a42029d9835fe97214d61f6397a4549bef
superhakar <43088948+superhakar@users.noreply.github.com>
2020-11-18 22:02:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,11 @@ This app will classify emails to different categories!
 # st.sidebar.header('Hyperparameter Tuning')
 
 funcs = {"Upload" : fun.upload,
-        "EDA" : fun.eda,
         "Train Parameters" : fun.trainit,
         "Test" : fun.test}
     
 def run():
-    option = st.sidebar.radio("Select an option",("Upload","EDA","Train Parameters","Test"))
+    option = st.sidebar.radio("Select an option",("Upload","Train Parameters","Test"))
     funcs[option]()
 
 if __name__ == "__main__":
