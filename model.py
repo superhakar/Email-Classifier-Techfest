57ddfe939b7bdf507bd21d0b6215d9cf5e1be9d9
superhakar <43088948+superhakar@users.noreply.github.com>
2020-12-16 13:53:07 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,166 +1,282 @@
-import streamlit as st
-import pandas as pd
-from wordcloud import WordCloud, STOPWORDS 
-import matplotlib.pyplot as plt 
 import numpy as np
-import os, zipfile
-import email
-from sklearn.datasets import make_blobs
-import model
-import time
-import plotly.graph_objects as go
-import glob
-import extract_msg as em
-import base64
+import pandas as pd
+import re, string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt 
+import streamlit as st
+from sklearn.model_selection import cross_val_score
+from sklearn.cluster import Birch
+import os
+from scipy.sparse import vstack,csr_matrix
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from scipy.sparse import hstack
+from xgboost import XGBClassifier
 
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-def upload():
-    uploaded_file = st.file_uploader("Upload your input zip folder", type=["zip"])
-    # upload_link = st.text_input("Upload From Link")
-    if uploaded_file is not None:
-        if(zipfile.is_zipfile(uploaded_file)):
-            with zipfile.ZipFile(uploaded_file,"r") as zf:
-                zf.extractall(path="Dataset")
-    if(len(glob.glob('Dataset/*')) != 0):
-        data,code = convert()
-        model.preprocess(data,'train')
-        wc = st.selectbox('Word clouds',code)
-        wordc(wc,code)
-        model.SVD(code)
-        f = open("Code/code.txt",'w+')
-        for i in range(len(code)):
-            f.write("%s\r\n" % code[i])
-        f.close()
-
-def convert():
-    fold = glob.glob('Dataset/*')
-    code = []
-    all_message = []
-    for i in fold:
-        code.append(i.replace("Dataset/",''))
-        messages = []
-        for j in glob.glob(i+'/*'):
-            msg = em.Message(j)
-            s = str(msg.subject) + str(msg.body)
-            messages.append(s)
-        all_message.append(messages)
-
-    df = pd.DataFrame(columns=['mail','class'])
-    for i in range(len(all_message)):
-        new_df = pd.DataFrame(columns=['mail','class'])
-        new_df['mail'] = all_message[i]
-        new_df['class'] = np.zeros(len(all_message[i]))+i*np.ones(len(all_message[i]))
-        df = pd.concat([df,new_df],ignore_index = True)
-    return df,code
-
-def converttest():
-    test = glob.glob('Testset/*')
-    test[0].replace("Testset/",'')
-    testfold = glob.glob(test[0]+'/*')
-    test_messages = []
-    name = []
-    for i in testfold:
-        name.append(i)
-        msg = em.Message(i)
-        s = str(msg.subject) + str(msg.body)
-        test_messages.append(s)
-
-    for i in range(len(name)):
-        name[i]=name[i].replace(test[0]+'/','')
-
-    df = pd.DataFrame()
-    df['mail'] = test_messages
-    df['name'] = name
-    return df
-
-def wordc(wc,code):  
-    df = pd.read_csv(r"CSV/Cleaned_Mails.csv", encoding ="latin-1") 
-    comment_words = '' 
-    stopwords = set(STOPWORDS) 
-    # iterate through the csv file 
-    for val in df.loc[df['class'] == code.index(wc),'mails']: 
-        # typecaste each val to string 
-        val = str(val) 
-        # split the value 
-        tokens = val.split() 
-        # Converts each token into lowercase 
-        for i in range(len(tokens)): 
-            tokens[i] = tokens[i].lower() 
-        comment_words += " ".join(tokens)+" "
-    wordcloud = WordCloud(width = 800, height = 800, 
-                    background_color ='white', 
-                    stopwords = stopwords, 
-                    min_font_size = 10).generate(comment_words) 
-    # plot the WordCloud image                        
-    plt.figure(figsize = (8, 8), facecolor = None) 
-    plt.imshow(wordcloud) 
-    plt.axis("off") 
-    plt.tight_layout(pad = 0) 
-    st.pyplot() 
-            
+TOKENIZER = re.compile(f'([“”¨«»®´·º½¾¿¡§£₤‘’])')
+def tokenize(s):
+  return TOKENIZER.sub(r' \1 ', s).split()
 
 
-def trainit():
-    
-    with st.beta_expander("Model 1 - TF-IDF Hyperparameters"):
-        st.header("Model 1 - TF-IDF")
-        h1 = st.slider('N-gram', 1,10,2)
-        h2 = st.text_input("N-Estimators", "100")
-    placeholder1 = st.empty()
-    placeholder2 = st.empty()
+def SVD(code):
+  df = pd.read_csv('CSV/Cleaned_Mails.csv')
+  TOKENIZER = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+  def tokenize(s):
+    return TOKENIZER.sub(r' \1 ', s).split()
+  tfidf_vectorizer = TfidfVectorizer(
+        ngram_range=(1,1),
+        tokenizer=tokenize,
+        min_df=3,
+        max_df=0.9,
+        strip_accents='unicode',
+        use_idf=True,
+        smooth_idf=True,
+        sublinear_tf=True
+    ).fit(df['mails'])
+  X = tfidf_vectorizer.transform(df['mails'])
+  tsvd = TruncatedSVD(n_components=2)
+  tsvd.fit(X)
+  X_tsvd = tsvd.transform(X)
+  for j in range(len(df['class'].unique())):
+    plt.scatter(X_tsvd[df['class'] == j, 0], X_tsvd[df['class'] == j, 1], label = code[j])
+  plt.legend()
+  st.write("Classified plot:")
+  st.pyplot()
 
-    with st.beta_expander("Model 2 - LSTM Hyperparameters"):
-        st.header("Model 2 - LSTM")
-        h3 = st.slider('Embedding_dim', 16,40,20)
-        h4 = st.slider('vocab_size', 1000,2000,1800)
-        h5 = st.slider('max_length', 80,140,120)
-        h6 = st.text_input("num_epochs", "25")
-    placeholder3 = st.empty()
-    placeholder4 = st.empty()
+def preprocess(data,met):
+  def clean(text):   
+    # removing paragraph numbers
+    text = re.sub('[0-9]+.\t','',str(text))
+    # removing new line characters
+    text = re.sub('\r',' ',str(text))
+    text = re.sub('\t',' ',str(text))
+    # text = re.sub('\n',' ',str(text))
+    # removing apostrophes
+    text = re.sub("'s",'',str(text))
+    # removing hyphens
+    text = re.sub("-",' ',str(text))
+    text = re.sub("— ",'',str(text))
+    text = re.sub(":",'',str(text))
+    # removing quotation marks
+    text = re.sub('\"','',str(text))
+    # removing salutations
+    text = re.sub("Mr\.",'Mr',str(text))
+    text = re.sub("Mrs\.",'Mrs',str(text))
+    # removing any reference to outside text
+    text = re.sub("[\(\[].*?[\)\]]", "", str(text))
+    # removing numbers
+    text = re.sub("\d+", "", str(text))
+    # removing email ids
+    text = re.sub("\S*@\S*\s?", "", str(text))
+    text = re.sub(r'[^\w\s]', ' ', str(text)) 
+    return text
+  
+  def get_text(Series, row_num_slicer):
+    """returns a Series with text sliced from a list split from each message. Row_num_slicer
+    tells function where to slice split text to find only the body of the message."""
+    result = pd.Series(index=Series.index, dtype = Series.dtypes)
+    for row, message in enumerate(Series):
+        message_words = message.split('\n')
+        del message_words[:row_num_slicer]
+        result.iloc[row] = message_words
+    return result
 
-   
-    if(st.button("Train")):
-        with st.spinner(text='Training...'):
-            cv,acc,loss = model.train(h1,int(h2),h3,h4,h5,int(h6))
-            # time.sleep(5)
-        placeholder2.success("Cross Validation Score: %.5f" % cv)
-        placeholder3.success("Training accuracy: %.5f" % acc)
-        placeholder4.success("Loss: %.5f" % loss)
-        # placeholder3.success("accuracy: 99%")
+  def agg(lst):
+    result = ''
+    for i in lst:
+        result = result + str(i)
+    return result
 
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}"><input type="button" value="Download {file_label}"></a>'
-    return href
+  data['new_mails'] = get_text(data['mail'], 4)
+  data['new_mails'] = data['new_mails'].apply(agg)
+  data['cleaned_mails'] = data['new_mails'].apply(clean)
+  train_mails = data['cleaned_mails'].tolist()
+  stop_words = set(stopwords.words('english')) 
+  stop_words = list(stop_words)
+  stop_words = list(stop_words) + ['regards', 'thank', 'abc', 'xyz', 'test', 'sorry', 'thanks', 'to', 'from', 'sent', 'recieved']
+  mails = []
+  for mail in train_mails:
+    sent = ''
+    l_words = mail.split()
+    for word in l_words:
+        word1 = word.lower()
+        if word1 not in stop_words:
+            sent = sent + str(word1) + ' '
+    mails.append(sent[:-1]) 
+  if (met == 'train'):
+    new_df = pd.DataFrame(columns=['mails','class'])
+    new_df['mails'] = mails
+    new_df['class'] = data['class']
+    new_df.to_csv('CSV/Cleaned_Mails.csv')
+  else:
+    new_df = pd.DataFrame(columns=['mails','name'])
+    new_df['mails'] = mails
+    new_df['name'] = data['name']
+    new_df.to_csv('CSV/Test_Cleaned_Mails.csv')
+
+def export_new_feature_matrix(data_corpus):
+  sensitive_words = ['change', 'address', 'street', 'detail', 'road', 'customer', 'support', 
+  'transfer', 'scheme', 'member', 'pension', 'information', 'fund', 'retirement', 'insurance', 'corp', 'payment', 
+  'update', 'centre', 'account', 'value', 'client', 'information', 'request', 'benefit', 'national', 'lump', 'sum', 'age']
+
+  # data corpus is list of strings
+  feature_matrix = np.zeros((len(data_corpus), len(sensitive_words)))
+  for j in range(len(data_corpus)):
+    temp_string = data_corpus[j]
+    list_of_words = temp_string.split()
+    for i in range(len(sensitive_words)):
+      if sensitive_words[i] in list_of_words:
+        feature_matrix[j][i] = 100
+  return feature_matrix
+
+def train(ngram=2,n_estimators=100,embedding_dim=20,vocab_size=1800,max_length=120,num_epochs=25):
+  dataset = pd.read_csv('CSV/Cleaned_Mails.csv')
+  tfidf = TfidfVectorizer(
+        ngram_range=(1,ngram),
+        tokenizer=tokenize,
+        min_df=3,
+        max_df=0.9,
+        strip_accents='unicode',
+        use_idf=True,
+        smooth_idf=True,
+        sublinear_tf=True
+    ).fit(dataset['mails'])
+  X = tfidf.transform(dataset['mails']).toarray()
+  y = dataset.iloc[:, -1].values
+  fvs = export_new_feature_matrix(dataset['mails'])
+  new_X = hstack([X, csr_matrix(fvs)]).toarray()
+  pickle.dump(tfidf, open('Model/tfidf.sav', 'wb'))         
+  classifier = XGBClassifier(n_estimators=n_estimators)
+  classifier.fit(new_X,y)
+  filename = 'Model/model.sav'
+  pickle.dump(classifier, open(filename, 'wb'))
+  model = RandomForestClassifier()
+  scores = cross_val_score(model, X, y, cv=5)
+  crossval = np.mean(scores)
+
+  trunc_type='post'
+  padding_type='post'
+  oov_tok = "<OOV>"
+  # training_size = 20000
+  tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+  tokenizer.fit_on_texts(dataset['mails'])
+  training_sequences = tokenizer.texts_to_sequences(dataset['mails'])
+  training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+  filename = 'Model/token.sav'
+  pickle.dump(tokenizer, open(filename, 'wb'))
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(50, return_sequences = True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(40)),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+  ])
+  scce = tf.keras.losses.SparseCategoricalCrossentropy()
+  model.compile(loss=scce,optimizer='adam',metrics=['accuracy'])
+  training_padded = np.array(training_padded)
+  training_labels = np.array(dataset['class'])
+  history = model.fit(training_padded, training_labels, epochs=num_epochs, verbose=1)
+  model.save('Model/')
+  return crossval,history.history['accuracy'][-1],history.history['loss'][-1]
+
+def test(dissimilar):
+  loaded_tfidf = pickle.load(open('Model/tfidf.sav', 'rb'))
+  filename = 'Model/model.sav'
+  loaded_model = pickle.load(open(filename, 'rb'))
+  test = pd.read_csv('CSV/Test_Cleaned_Mails.csv')
+  X_test1 = loaded_tfidf.transform(test['mails']).toarray()
+  fvs = export_new_feature_matrix(test['mails'])
+  new_X = hstack([X_test1, csr_matrix(fvs)]).toarray()
+  y_pred1 = loaded_model.predict_proba(new_X)
+  loaded_token = pickle.load(open('Model/token.sav', 'rb'))
+  loaded_modellstm = tf.keras.models.load_model('Model/')
+  max_length = 120
+  trunc_type='post'
+  padding_type='post'
+  testing_sequences = loaded_token.texts_to_sequences(test['mails'])
+  testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+  testing_padded = np.array(testing_padded)
+  results = loaded_modellstm.predict(testing_padded)
+  y_pred2 = np.array(results)
+  f = open("Code/code.txt",'r')
+  f1 = f.readlines()
+  code=[]
+  for i in f1:
+    code.append(i.replace("\n",""))
+  f.close()
+  # y_pred3 = np.zeros(y_pred2.shape)
+  # print(y_pred3.shape)
+  # print("transfer" in test['mails'])
+  # for i in range(len(test['mails'])):
+  #   if("transfer" in test['mails'][i]):
+  #     y_pred3[i][code.index("Transfers")]=1
+  #   if("change" in test['mails'][i]):
+  #     y_pred3[i][code.index("MDU")]=1
+  # print(y_pred3)
+  # print(y_pred2)
+  # print(y_pred1)
+  y_pred = y_pred1*0.9 + y_pred2*0.1
+  print(y_pred)
+  y_pred = np.argmax(y_pred, axis=1)
+  df = pd.DataFrame()
+  df['File Name'] = test['name']
+  ystr = []
+  for i in range(len(y_pred)):
+    ystr.append(code[int(y_pred[i])])
+  df['Classified Category'] = ystr
+  df['Confidence'] = dissimilar
+  df.to_csv('Result/result.csv', index = False)
 
 
-def test():
-    uploaded_file = st.file_uploader("Upload your Test file/zip folder", type=["zip"])
-    if uploaded_file is not None:
-        if(zipfile.is_zipfile(uploaded_file)):
-            with zipfile.ZipFile(uploaded_file,"r") as zf:
-                zf.extractall(path="Testset")
-        data = converttest()
-        model.preprocess(data,test)
-        score,xtrain2d,xtest2d,ins,out, dissimilar = model.Similarity()
-        if(st.button("Test")):
-            model.test(dissimilar)
-            st.success("Done!!")
-            st.markdown(get_binary_file_downloader_html('Result/result.csv', 'Result'), unsafe_allow_html=True)
-            # st.markdown(get_binary_file_downloader_html('Test.zip', 'Result folder'), unsafe_allow_html=True)
-        fig = go.Figure(go.Indicator(
-        mode = "gauge+number",
-        value = score,
-        gauge = {'axis': {'range': [None, 100]}},
-        domain = {'x': [0, 1], 'y': [0, 1]},
-        title = {'text': "Confidence Score"}))
-        st.plotly_chart(fig)
-        plt.plot(xtrain2d[:, 0], xtrain2d[:, 1], '+',label="train")
-        plt.plot(xtest2d[ins, 0], xtest2d[ins, 1], '*',label="test inside")
-        plt.plot(xtest2d[out, 0], xtest2d[out, 1], 'x',label="test outside")
-        plt.legend()
-        st.pyplot() 
+def Similarity():
+  train_df = pd.read_csv('CSV/Cleaned_Mails.csv') # change **
+  test_df = pd.read_csv('CSV/Test_Cleaned_Mails.csv') # change **
+  TOKENIZER = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+  def tokenize(s):
+      return TOKENIZER.sub(r' \1 ', s).split()
+
+  tfidf_vectorizer = TfidfVectorizer(
+          ngram_range=(1, 1),
+          tokenizer=tokenize,
+          min_df=3,
+          max_df=0.95,
+          strip_accents='unicode',
+          use_idf=True,
+          smooth_idf=True,
+          sublinear_tf=True,
+          max_features=1000
+      ).fit(pd.concat([train_df['mails'] , test_df['mails']],ignore_index = True))
+
+  xtrain = tfidf_vectorizer.transform(train_df['mails']) 
+  xtest = tfidf_vectorizer.transform(test_df['mails'])
+  birch = Birch(n_clusters=None, threshold=0.9, branching_factor=50)
+  birch.fit(xtrain)
+  birch.partial_fit(xtest)
+  set_diff = set(np.unique(birch.predict(xtest))) - set(np.unique(birch.predict(xtrain))) 
+  count = 0
+  out = []
+  ins = []
+  dissimilar = np.zeros(test_df.values.shape[0])
+  for j, i in enumerate(birch.predict(xtest)):
+      if i in set_diff:
+          count += 1
+          out.append(j)
+      else:
+          ins.append(j)
+
+  sim_score = 1 - count/xtest.shape[0]
+  out, ins = np.asarray(out, dtype=int), np.asarray(ins, dtype=int)
+  dissimilar[ins] += 1
+  tsvd = TruncatedSVD(n_components=2)
+  tsvd.fit(vstack([xtrain, xtest], format='csr'))
+  xtrain2d = tsvd.transform(xtrain)
+  xtest2d = tsvd.transform(xtest)
+  return sim_score*100,xtrain2d,xtest2d,ins,out, dissimilar
